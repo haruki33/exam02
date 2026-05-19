7e6eb8932e651de88c734e81610b2aea5bf345df
haruki33 <kkhk0869hs33@gmail.com>
2026-05-19 14:15:47 +0900

    足し算を行う機能の実装が完了
    足し算を行うコントローラークラスを実装
    
    return "exam02-result2";の余分な部分を削除
    
    yamlにデータベースとアプリケーションのパスを追加
    
    足し算を入力するhtmlを実装
    
    最初の足し算結果画面の実装
    
    一回目の結果画面から呼ばれる二回目の結果画面の実装
    
    sessionスコープから取得するように変更
    
    sessionスコープから取得するように変更
    
    ブラウザごとに管理するため、sessionスコープに修正

---
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam02")
+public class Exam02Controller {
+
+    @Autowired
+    private HttpSession session;
+
+    @GetMapping("")
+    public String index() {
+        return "exam02";
+    }
+
+    @PostMapping("/result")
+    public String result(Integer num1, Integer num2) {
+        session.setAttribute("num1", num1);
+        session.setAttribute("num2", num2);
+        session.setAttribute("sum", num1 + num2);
+        return "exam02-result";
+    }
+
+    @GetMapping("/result2")
+    public String result2() {
+        return "exam02-result2";
+    }
+}
